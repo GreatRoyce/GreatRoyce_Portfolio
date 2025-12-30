9bedb0eae3db45afa2853fd2d5ea1a3a8b92ab88
GreatRoyce <royceokoh@gmail.com>
2025-12-30 09:22:10 +0100

    harmonized-project-cloudinary

--- backend/src/multer/multer.js
+++ backend/src/multer/multer.js
@@ -59,7 +59,7 @@
 
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("../../config/cloudinary");
 
 
 const storage = new CloudinaryStorage({
